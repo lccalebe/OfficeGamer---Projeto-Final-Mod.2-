4d25f6100290e488ae46df769e9a8e0b24b798f9
Guilherme Carvalho <carvalho_guilhermegomes@hotmail.com>
2022-02-11 18:16:20 -0300

    alteração no erro do input

--- js/login.js
+++ js/login.js
@@ -17,7 +17,7 @@ let msgConfirrm = document.querySelector("#msgn-confirm");
 
 function validaForm(){
     if(nome == false  || sobrenome == false  || email == false || senha == false || confirrm == false || cb.checked == false){
-        erro.innerHTML = `tente novamente`
+        erro.innerHTML = "tente novamente"
     }else{
         register.innerHTML = "usuario cadastrado com sucesso";
         setTimeout(() => {
@@ -32,49 +32,53 @@ submit.addEventListener('click', function(event){
     
         if(nome.value.length < 3){
             nome.classList.add("err");
-            msgNome.innerText = "Nome inválido"
-        }else{
-            nome.classList.add("ok");
-            console.log(nome.value)
-        }
+            msgNome.innerText = "Nome inválido";
+            return false
+        }   nome.classList.add("ok");
+            msgNome.innerText = "";
+            
+        
 
         if(sobrenome.value.length < 3){
             nome.classList.add("err");
             msgSobreNome.innerText = "Sobrenome inválido"
-        }else{
-            nome.classList.add("ok");
-            console.log(sobrenome.value)
-        }
+            return false
+        }   nome.classList.add("ok");
+            msgSobreNome.innerText = "";
+        
+        
 
         if(email.value.indexOf("@") == -1 || email.value.indexOf(".") == -1){
             email.classList.add("err");
             msgEmail.innerText = "E-mail inválido";
-        }else{
-            email.classList.add("ok");
-            console.log(email.value)
-        }
+            return false
+        }   email.classList.add("ok");
+            msgEmail.innerText = "";
+       
+        
        
         if(senha.value.length < 8){
             senha.classList.add("err");
             msgSenha.innerText = "Senha inválida";
-            
-        }else{
-            senha.classList.add("ok");
-            console.log(senha.value)
-        }
+            return false
+        }   senha.classList.add("ok");
+            msgSenha.innerText = "";
+        
 
-        if(confirrm.value === senha.value){
-            confirrm.classList.add("ok");
-            console.log(confirrm.value)
-        }else{
-            confirrm.classList.add("err");
+        if(confirrm.value.length !== senha.value.length || confirrm.value.length == ""){
             msgConfirrm.innerText = "Senha incorreta";
-        }
+            confirrm.classList.add("err");
+            return false
+        }   confirrm.classList.add("ok");
+            msgConfirrm.innerText = "";
+
 
         if(cb.checked == false){
             erro.innerHTML = `é importante ler e aceitar os termos de uso.`;
+            return false
         }
+        erro.innerHTML = ``;
 
-        validaForm();
+        return validaForm();
 });
 
