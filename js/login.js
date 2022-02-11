9a0046f3571a1228ab9add37464b4e388a9d34c7
Guilherme Carvalho <carvalho_guilhermegomes@hotmail.com>
2022-02-11 16:51:49 -0300

    mudança no form

--- js/login.js
+++ js/login.js
@@ -6,50 +6,75 @@ let confirrm = document.querySelector("#confirm-password")
 let email= document.querySelector("#email");
 let sobrenome = document.querySelector("#sobrenome");
 let cb = document.querySelector("#checkbox");
+let register = document.querySelector("#register");
+let form = document.querySelector("#register-form");
+let info = document.querySelector(".info");
+let msgEmail = document.querySelector("#msgn-email");
+let msgNome = document.querySelector("#msgn-nome");
+let msgSobreNome = document.querySelector("#msgn-sobrenome");
+let msgSenha = document.querySelector("#msgn-senha");
+let msgConfirrm = document.querySelector("#msgn-confirm");
 
+function validaForm(){
+    if(nome == false  || sobrenome == false  || email == false || senha == false || confirrm == false || cb.checked == false){
+        erro.innerHTML = `tente novamente`
+    }else{
+        register.innerHTML = "usuario cadastrado com sucesso";
+        setTimeout(() => {
+            window.location.href = "./teste.html"
+        }, 2000);
+    }
+}
 
 submit.addEventListener('click', function(event){
     event.preventDefault();
 
-    try {
-        if(nome.value == "" || nome.value.length < 3){
-            throw `Digite um nome válido.`;
+    
+        if(nome.value.length < 3){
+            nome.classList.add("err");
+            msgNome.innerText = "Nome inválido"
+        }else{
+            nome.classList.add("ok");
+            console.log(nome.value)
         }
 
-        if(sobrenome.value == "" || sobrenome.value.length < 3){
-            throw `Digite um sobrenome válido.`;
+        if(sobrenome.value.length < 3){
+            nome.classList.add("err");
+            msgSobreNome.innerText = "Sobrenome inválido"
+        }else{
+            nome.classList.add("ok");
+            console.log(sobrenome.value)
         }
 
         if(email.value.indexOf("@") == -1 || email.value.indexOf(".") == -1){
-            throw `E-mail inválido.`;
+            email.classList.add("err");
+            msgEmail.innerText = "E-mail inválido";
+        }else{
+            email.classList.add("ok");
+            console.log(email.value)
         }
-
        
         if(senha.value.length < 8){
-            throw `faça uma senha com 8 caracteres.`;
+            senha.classList.add("err");
+            msgSenha.innerText = "Senha inválida";
+            
         }else{
-            erro.innerHTML = "";
+            senha.classList.add("ok");
+            console.log(senha.value)
         }
 
-        if(confirrm.value !== senha.value){
-            throw `Sua senha não está correta.`
+        if(confirrm.value === senha.value){
+            confirrm.classList.add("ok");
+            console.log(confirrm.value)
+        }else{
+            confirrm.classList.add("err");
+            msgConfirrm.innerText = "Senha incorreta";
         }
 
-        //if(cb.value !== true){
-        //    throw `é importante ler e aceitar os termos de uso.`
-        //}else{
-        //    erro.innerHTML = "";
-        //}
+        if(cb.checked == false){
+            erro.innerHTML = `é importante ler e aceitar os termos de uso.`;
+        }
 
-        window.location.href = "./teste.html"
-        
-    } 
-    
-    catch (error) {
-        erro.innerHTML = error;
-    }
-
-
-
+        validaForm();
 });
 
