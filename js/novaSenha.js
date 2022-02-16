31c6c582c5f799966f33a7dfd05df5d4005b8374
André Rodrigues <rodrigues.domingos996@gmail.com>
2022-02-16 00:33:43 -0300

    correção da logica novaSenha

--- js/novaSenha.js
+++ js/novaSenha.js
@@ -5,25 +5,33 @@ $("#enviar").click(function(event){
     var senha = $("#password").val();
     var confirmaSenha = $("#nova-password").val();
 
+    
+
     if(senha != confirmaSenha ){
 
         document.getElementById("msgn-senha").innerHTML = "Senhas estão diferentes";
+        var senhaIgual = false;
 
     } else {
        
         document.getElementById("msgn-senha").innerHTML = "";
-      
-        if(confirmaSenha.length < 8){
-    
-            document.getElementById("msgn-senha2").innerHTML = "senha muito curta";
-    
-        } else {
-            document.getElementById("msgn-senha2").innerHTML = ""
-        }
-
-        window.location.href = "index.html";
+        var senhaIgual = true;
     }
     
+    if(confirmaSenha.length < 8){
 
+        document.getElementById("msgn-senha2").innerHTML = "senha muito curta";
+        var tamanhoSenha = false;
 
+    } else {
+        document.getElementById("msgn-senha2").innerHTML = ""
+        var tamanhoSenha = true;
+    }
+
+    if(senhaIgual == true && tamanhoSenha == true){
+
+    window.location.href = "index.html";
+    
+    }
+    
 })
